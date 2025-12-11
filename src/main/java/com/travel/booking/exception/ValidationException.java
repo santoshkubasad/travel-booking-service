a07ed70cd2ddc81d4b4c2999a1c3edc03a0c8a90
santoshkubasad <92029880+santoshkubasad@users.noreply.github.com>
2025-12-11 15:54:05 +0530

    Add files via upload

---
+++ src/main/java/com/travel/booking/exception/ValidationException.java
@@ -0,0 +1,12 @@
+package com.travel.booking.exception;
+
+public class ValidationException extends RuntimeException {
+    
+    public ValidationException(String message) {
+        super(message);
+    }
+    
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
